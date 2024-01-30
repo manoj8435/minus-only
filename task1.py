44fb3e25c9febe2cf95de996542da1b3c3403c36
Manoj Soni <manojsoni.bhu@gmail.com>
2024-01-30 23:15:09 +0530

    testing

---
+++ task1.py
@@ -0,0 +1,14 @@
+intA[]={5,6,0,4,6,0,9,8}
+int n = sizeof(A)/sizeof(A[0])
+int j=0
+for (int i = 0; i<n;i++){
+    if(A[i]!0){
+        swap(A[j],A[i]);
+        j++
+        
+    }
+    (int i=0; i<n; i++){
+        count<<[A]<<" ";
+    }
+}
+
